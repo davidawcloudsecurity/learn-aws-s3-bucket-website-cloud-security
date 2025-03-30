c20d9bddb9b49e2b2ec3c9cca87d15a7ea7223ea
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-03-30 13:38:57 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "random_id" "bucket_suffix" {
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "davidawcloudsecurity-${random_id.bucket_suffix.hex}"  # Default bucket name
+  default     = "davidawcloudsecurity-$(random_id.bucket_suffix)"
 }
 
 variable "region" {
